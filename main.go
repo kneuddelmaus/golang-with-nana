aa06d063b99351fe0029aa5875a2a8a853d39fcc
kneuddelmaus <baldwin403@protonmail.com>
2022-05-01 12:49:31 +0530

    [kneuddlemaus]: Adds certain validations

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	const (
@@ -11,25 +14,49 @@ func main() {
 	fmt.Println("Welcome to", conferenceName, "ticket booking application")
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
+	var bookedNames []string
 
-	var (
-		firstName   string
-		lastName    string
-		email       string
-		userTickets uint8
-	)
+	for {
+		var (
+			firstName   string
+			lastName    string
+			email       string
+			userTickets uint8
+		)
 
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email address")
-	fmt.Scan(&email)
-	fmt.Println("Enter the number of tickets you intend to purchase")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
+		fmt.Println("Enter the number of tickets you intend to purchase")
+		fmt.Scan(&userTickets)
 
-	remainingTickets -= userTickets
+		if userTickets > remainingTickets {
+			fmt.Printf("Only %v tickets can be booked\n", remainingTickets)
+			continue
+		} else {
+			bookedNames = append(bookedNames, firstName+" "+lastName)
+			remainingTickets -= userTickets
 
-	fmt.Printf("Thank you %v %v for purchasing %v tickets for %v\n", firstName, lastName, userTickets, conferenceName)
-	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for purchasing %v tickets for %v\n", firstName, lastName, userTickets, conferenceName)
+			fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+
+			var firstNames []string
+			for _, booking := range bookedNames {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("The booking are made by %v \n", firstNames)
+		}
+
+		if remainingTickets == 0 {
+			fmt.Printf("Tickets for %v are sold out", conferenceName)
+			break
+		} else {
+			continue
+		}
+	}
 }
