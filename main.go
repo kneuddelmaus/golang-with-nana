8ac3521ffcfb78a115ab4d6c176fd935004d60fd
kneuddelmaus <baldwin403@protonmail.com>
2022-05-01 11:29:10 +0530

    [kneuddlemaus]: Sets types for const and vars

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	const (
-		conferenceName    = "Go Conference"
-		conferenceTickets = 50
+		conferenceName    string = "Go Conference"
+		conferenceTickets uint8  = 50
 	)
-	var remainingTickets = conferenceTickets
+	var remainingTickets uint8 = conferenceTickets
 	fmt.Println("Welcome to", conferenceName, "ticket booking application")
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
