f909b3472339f3f288cc208d4ed482d42b739fdb
kneuddelmaus <baldwin403@protonmail.com>
2022-05-01 11:50:57 +0530

    [kneuddlemaus]: Gets user input using Scan() without error handling

--- main.go
+++ main.go
@@ -11,4 +11,25 @@ func main() {
 	fmt.Println("Welcome to", conferenceName, "ticket booking application")
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
+
+	var (
+		firstName   string
+		lastName    string
+		email       string
+		userTickets uint8
+	)
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
+	fmt.Println("Enter the number of tickets you intend to purchase")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+
+	fmt.Printf("Thank you %v %v for purchasing %v tickets for %v\n", firstName, lastName, userTickets, conferenceName)
+	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 }
